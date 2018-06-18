53d34d14d14991b9aa666b5a8db29d72dffd8a2c
chenying1 <cy_ariel@163.com>
2018-06-18 16:11:47 +0800

    set mysql and time_zone

--- bi_monitor/settings.py
+++ bi_monitor/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'bi_monitor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cy',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
