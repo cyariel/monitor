bd9a58fc7289096ea5347026de455f31ce96ae41
chenying1 <cy_ariel@163.com>
2018-06-19 12:26:27 +0800

    create superuser

--- bi_monitor/settings.py
+++ bi_monitor/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bi_monitor_app.apps.BiMonitorAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
